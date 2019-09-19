98236dcc6278d6f21c8f75d562e134b130e607fc
Mark McFate <summitt.dweller@gmail.com>
2019-09-18 21:13:27 -0500

    Update push-update.sh

--- push-update.sh
+++ push-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd ~/Projects/weblog-sd
+cd ~/Projects/weblog-m4ck3n
 perl -i.bak -lpe 'BEGIN { sub inc { my ($num) = @_; ++$num } } s/(build = )(\d+)/$1 . (inc($2))/eg' config.toml
-docker image build -t blog-update .
+docker image build -t blog-m4-update .
 docker login
-docker tag blog-update summittdweller/weblog-sd:latest
-docker push summittdweller/weblog-sd:latest
+docker tag blog-m4-update summittdweller/weblog-m4ck3n:latest
+docker push summittdweller/weblog-m4ck3n:latest
